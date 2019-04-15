d9d5cf73611088f47ee21d8ddb84c36dd75f8f0e
Phoomparin Mano <phoomparin@gmail.com>
2019-04-15 23:18:55 +0700

    :sparkles: Add Cube

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,25 @@
+import processing.core.PConstants._
 import processing.core._
 import processing.opengl.PShader
 
 class Sketch extends PApplet {
+  var cube: PShape = _
   var blueShader: PShader = _
+  var angle: Float = 0.0f
 
   override def settings(): Unit = {
-    size(500, 500, PConstants.P3D)
+    size(500, 500, P3D)
     pixelDensity(2)
   }
 
   override def setup(): Unit = {
     blueShader = loadShader("shader.frag")
+
+    cube = createShape(BOX, 50)
+    cube.setStroke(color(80))
+    cube.setStrokeWeight(4)
+
+    rectMode(CENTER)
   }
 
   override def draw(): Unit = {
@@ -18,8 +27,23 @@ class Sketch extends PApplet {
 
     stroke(60)
     strokeWeight(3)
+    ellipse(mouseX, mouseY, 20, 20)
+
+    camera(width / 2, height / 2, mouseY,
+      width / 2, height / 2, 0,
+      0, 1, 0)
+
+    pointLight(200, 200, 200, width / 2, height / 2, -200)
+
+    translate(width / 2, height / 2)
+    rotateX(angle)
+    rotateY(angle)
+    rotateZ(angle)
+
     shader(blueShader)
-    ellipse(mouseX, mouseY, 200, 200)
+    shape(cube)
+
+    angle += 0.1f
   }
 }
 
